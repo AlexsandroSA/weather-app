4f4723d7b46a273cbf70af57e7fa55df621e0c27
Alexsandro <alexsandro.alves@fcamara.com.br>
2020-01-21 07:43:02 -0300

    feat(app): Add service weather

---
+++ src/api/services/weather.service.tsx
@@ -0,0 +1,19 @@
+import { apiUrl, apikey } from '../../helpers/envManager';
+
+
+export async function getWeatherByCoords(lat: number, lon: number) {
+    const data = await fetch(`${apiUrl}/weather?lat=${lat}&lon=${lon}&appid=${apikey}`)
+        .then(response => response.json())
+        .then(data => {
+            return {
+                temp: Math.round(data.main.temp -273.15),
+                type: data.weather[0].main,
+                name: data.name,
+                country: data.sys.country,
+            };
+        })
+        .catch(e => console.log(e));
+
+    console.log(data);
+    return data;
+}
