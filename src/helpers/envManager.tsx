33a0e33a1ddd25f92344bfd4c1e318f9c9c5416e
Alexsandro <alexsandro.alves@fcamara.com.br>
2020-01-21 07:43:28 -0300

    feat(app): Add helper envManager

---
+++ src/helpers/envManager.tsx
@@ -0,0 +1,5 @@
+export const appVersion = process.env.REACT_APP_VERSION || '';
+export const appName = process.env.REACT_APP_NAME || 'Weather App';
+
+export const apiUrl = process.env.REACT_APP_API_URL || '';
+export const apikey = process.env.REACT_APP_API_KEY || '';
