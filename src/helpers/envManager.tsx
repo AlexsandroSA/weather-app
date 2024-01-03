2263dbbc38efb19fa3db178f8b440053aad0d9d7
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2024-01-03 00:18:19 -0300

    chore: setup vite and remove react scripts

--- src/helpers/envManager.tsx
+++ src/helpers/envManager.tsx
@@ -1,5 +1,5 @@
-export const appVersion = process.env.REACT_APP_VERSION || '';
-export const appName = process.env.REACT_APP_NAME || 'Weather App';
+export const appVersion = import.meta.env.VITE_VERSION || "";
+export const appName = import.meta.env.VITE_NAME || "Weather App";
 
-export const apiUrl = process.env.REACT_APP_API_URL || '';
-export const apikey = process.env.REACT_APP_API_KEY || '';
+export const apiUrl = import.meta.env.VITE_API_URL || "";
+export const apikey = import.meta.env.VITE_API_KEY || "";
