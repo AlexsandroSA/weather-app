469700ab0c3803dc9017cdce4de2e36935caf95d
Alexsandro Silva Alves <alexsando.s.alves@gmail.com>
2024-01-26 13:15:53 -0300

    chore: setup e2e

--- src/helpers/envManager.tsx
+++ src/helpers/envManager.tsx
@@ -1,5 +1,6 @@
 export const appVersion = import.meta.env.VITE_VERSION || "";
 export const appName = import.meta.env.VITE_NAME || "Weather App";
+export const appUrl = import.meta.env.VITE_APP_URL || "";
 
 export const apiUrl = import.meta.env.VITE_API_URL || "";
 export const apikey = import.meta.env.VITE_API_KEY || "";
